13ac12654996c2ebf345321ba563cb98c4d45df0
s8189882 <s8189882@gmail.com>
2022-08-12 13:24:00 +0530

    Displaying Welcome to Line Comparison Computation Program message

---
+++ LineComparison/src/lineCompare/LineCompare.java
@@ -0,0 +1,11 @@
+package lineCompare;
+
+public class LineCompare {
+	
+	public static void main(String args[]) {
+
+		//Start with Displaying Welcome to Line Comparison Computation Program on Master Branch
+		System.out.println("Welcome to the Line Comparison Computation Program ");
+	}
+
+}
