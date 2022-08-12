d5f4bf45c6936996c1c668b460b36e4858bf5f23
s8189882 <s8189882@gmail.com>
2022-08-12 15:41:15 +0530

    Checking equality between the lengths of 2 lines

--- LineComparison/src/lineCompare/LineCompare.java
+++ LineComparison/src/lineCompare/LineCompare.java
@@ -6,11 +6,16 @@ public class LineCompare {
 	
 	public static void main(String args[]) {
 		
-		int x1 = 0;
-		int x2 = 0;
-		int y1 = 0;
-		int y2 = 0;
-		double length = 0.0;
+		int line1x1 = 0;
+		int line1x2 = 0;
+		int line1y1 = 0;
+		int line1y2 = 0;
+		int line2x1 = 0;
+		int line2x2 = 0;
+		int line2y1 = 0;
+		int line2y2 = 0;
+		double lengthLine1 = 0.0;
+		double lengthLine2 = 0.0;
 
 
 		
@@ -24,22 +29,45 @@ public class LineCompare {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println();
-		System.out.println("Using the Cartesian Coordinate System, enter the endpoints of the line");
+		System.out.println("Using the Cartesian Coordinate System, enter the endpoints of the Line 1");
 		System.out.println("Endpoint 1 (x1, y1)");
 		System.out.println("Enter the abscissa (x-coordinate) : ");
-		x1 = sc.nextInt();
+		line1x1 = sc.nextInt();
 		System.out.println("Enter the ordinate (y-coordinate) : ");
-		y1 = sc.nextInt();
+		line1y1 = sc.nextInt();
 		System.out.println("Endpoint 2 (x2, y2)");
 		System.out.println("Enter the abscissa (x-coordinate) : ");
-		x2 = sc.nextInt();
+		line1x2 = sc.nextInt();
 		System.out.println("Enter the ordinate (y-coordinate) : ");
-		y2 = sc.nextInt();
+		line1y2 = sc.nextInt();
 		
-		length = Math.sqrt((Math.pow((x2-x1), 2)+Math.pow((y2-y1), 2)));
-		System.out.println("Length of the Line is : " + length);
+		lengthLine1 = Math.sqrt((Math.pow((line1x2-line1x1), 2)+Math.pow((line1y2-line1y1), 2)));
+		System.out.println("Length of Line 1 is : " + lengthLine1);
+		
+		System.out.println();
+		System.out.println("Using the Cartesian Coordinate System, enter the endpoints of the Line 2");
+		System.out.println("Endpoint 1 (x1, y1)");
+		System.out.println("Enter the abscissa (x-coordinate) : ");
+		line2x1 = sc.nextInt();
+		System.out.println("Enter the ordinate (y-coordinate) : ");
+		line2y1 = sc.nextInt();
+		System.out.println("Endpoint 2 (x2, y2)");
+		System.out.println("Enter the abscissa (x-coordinate) : ");
+		line2x2 = sc.nextInt();
+		System.out.println("Enter the ordinate (y-coordinate) : ");
+		line2y2 = sc.nextInt();
+		
+		lengthLine2 = Math.sqrt((Math.pow((line2x2-line2x1), 2)+Math.pow((line2y2-line2y1), 2)));
+		System.out.println("Length of Line 2 is : " + lengthLine2);
+		
 				
-		
+		//check equality of two lines based on the end points, whether two lines are the equal
+		Double d1 = new Double(lengthLine1);
+		Double d2 = new Double(lengthLine2);
+		if (d1.equals(d2))
+            System.out.print("Line 1 and line 2 have Equal lengths.");
+        else
+            System.out.print("Line 1 and Line 2 do not have Equal lengths.");
 	}
 
 }
