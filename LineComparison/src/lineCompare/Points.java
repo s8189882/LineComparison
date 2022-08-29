64644731297d77a9ecc82cf9b2790f7a7112d1e0
s8189882 <s8189882@gmail.com>
2022-08-29 19:32:05 +0530

    Refractor Using Java OOPs Concepts of Line and Point

---
+++ LineComparison/src/lineCompare/Points.java
@@ -0,0 +1,20 @@
+package lineCompare;
+
+public class Points {
+
+	private int xCoordinate;
+	private int yCoordinate;
+
+	public int getxCoordinate() {
+		return xCoordinate;
+	}
+	public void setxCoordinate(int xCoordinate) {
+		this.xCoordinate = xCoordinate;
+	}
+	public int getyCoordinate() {
+		return yCoordinate;
+	}
+	public void setyCoordinate(int yCoordinate) {
+		this.yCoordinate = yCoordinate;
+	}
+}
